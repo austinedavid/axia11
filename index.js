81ee9eea2e5eb4c28d500eae9bc687593461dcb8
austinedavid <davmesoaustine@gmail.com>
2026-08-17 20:10:45 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import usersRoute from "./routes/users.route.js";
+import postRoute from "./routes/post.route.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+function connectDB() {
+  mongoose
+    .connect(process.env.DBURL)
+    .then(() => console.log("DB connected"))
+    .catch((error) => console.log(error.message));
+}
+
+const app = express();
+app.use(express.json());
+
+app.use(usersRoute);
+app.use(postRoute);
+
+app.listen(4000, () => {
+  connectDB();
+  console.log("app is running, change made ");
+});
