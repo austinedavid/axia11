1e335d276f4b29ec9ee6e1eb0cc1b881ad342fce
austinedavid <davmesoaustine@gmail.com>
2026-09-01 03:40:06 +0100

    working on it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import errorHandlingMiddleware from "./middlewares/errorhandling.middleware.js";
 dotenv.config();
+import cors from "cors";
 
 function connectDB() {
   mongoose
@@ -16,7 +17,17 @@ function connectDB() {
 const app = express();
 // universal middleware
 app.use(express.json());
+app.use(express.text({ type: "text/plain" }));
+app.use(express.text({ type: "application/javascript" }));
+app.use(express.text({ type: "text/html" }));
+app.use(express.text({ type: "application/xml" }));
+app.use(express.urlencoded());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
+app.post("/", (req, res) => {
+  console.log(req.body);
+  return res.json({ message: "successful" });
+});
 app.use(usersRoute);
 app.use(postRoute);
 
